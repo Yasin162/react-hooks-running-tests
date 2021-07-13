700ebb3ec794180e710d8f5645937a7d40ffea1b
Yasin <yasinabdussaburr@gmail.com>
2021-07-13 11:00:51 -0400

    commit tree a182f9d383b50bc1e13c6ffa4c0ae8c7e7848cdd

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Header from "./Header";
+import Article from "./Article";
 function App() {
-  return <div className="App">learn react!</div>;
+  return <><div className="App">learn react!</div>;
+  <Header />
+  <Article />
+  </>
 }
 
 export default App;
